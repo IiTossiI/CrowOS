f312d6089df7eaca86b9d6ef13a6abbcfade10b7
IiTossiI <94807008+IiTossiI@users.noreply.github.com>
2023-01-18 17:12:49 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,3 +1,7 @@
+computer = require("computer")
 fs = require("filesystem")
 fs.remove("/home/install.lua")
-print("works")
+f = io.open("/home/.shrc", "w")
+f:write("cd /home/\nwget -fq https://raw.githubusercontent.com/IiTossiI/CrowOS/main/init.lua\n./init.lua")
+f:close()
+computer.shutdown(true)

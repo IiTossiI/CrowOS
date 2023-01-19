278c2b76982223521754289196318f06b7ea6ef1
IiTossiI <94807008+IiTossiI@users.noreply.github.com>
2023-01-19 20:38:58 +0200

    Fix init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ fs.remove("/home/init.lua")
 f = net.request("https://crowos.glitch.me/main.lua")
 result = ""
 for line in f do
-  result = result .. line .. "\n"
+  result = result .. line
 end
 f = io.open("/home/main.lua", "w")
 f:write(result)
